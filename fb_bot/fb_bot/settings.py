6531409dcb7e3bfdf70a1d58226002b90bba9921
Arsh Panghal <panghalarsh@gmail.com>
2018-07-27 09:59:46 +0530

    Update settings.py

--- fb_bot/fb_bot/settings.py
+++ fb_bot/fb_bot/settings.py
@@ -34,6 +34,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
